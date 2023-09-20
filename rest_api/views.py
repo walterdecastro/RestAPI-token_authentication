ca60cd594de6add26e07f11e92d76df58f6fa10a
walterdecastro <walterdc1995@gmail.com>
2023-09-19 22:58:23 -0300

    Finished project. Token Secure implemtation in REST API

--- rest_api/views.py
+++ rest_api/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from register.models import Register
 from rest_api.serializers import RegisterModelSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class RegisterModelViewSet(ModelViewSet):
     queryset = Register.objects.all()
     serializer_class = RegisterModelSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
